122f80708191a8235bcde82c703b48bfcc2cb48e
polirritmico <ebray187@gmail.com>
2023-12-05 19:12:30 -0300

    First approach

---
+++ lua/lazy-local-patcher/config.lua
@@ -0,0 +1,31 @@
+---@class PatcherConfig
+local M = {}
+
+---@type PatcherOptions
+M.options = {}
+
+---@class PatcherOptions
+local defaults = {
+    lazy_path = vim.fn.stdpath("data") .. "/lazy/", -- directory where lazy install the plugins
+    patches_path = vim.fn.stdpath("config") .. "/patches/", -- directory where diff patches files are stored
+}
+
+---@param path string
+local check_paths = function(path)
+    local output = vim.fn.finddir(path)
+    if output == "" then
+        -- vim.notify("[lazy-local-patcher] Not found directory " .. path)
+        error("[lazy-local-patcher] Not found directory " .. path)
+    end
+end
+
+---@param options PatcherConfig?
+function M.setup(options)
+    M.options = vim.tbl_deep_extend("force", {}, defaults, options or {})
+    check_paths(M.options.lazy_path)
+    check_paths(M.options.patches_path)
+
+    require("lazy-local-patcher.patcher").create_group_and_cmd()
+end
+
+return M
---
+++ lua/lazy-local-patcher/init.lua
@@ -0,0 +1,7 @@
+local config = require("lazy-local-patcher.config")
+
+local M = {}
+
+M.setup = config.setup
+
+return M
